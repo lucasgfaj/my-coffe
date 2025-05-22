fec8c5b5550336610621f87bbab82a0602fa6b12
lucasgfaj <lucasgfajardo09@gmail.com>
2025-05-21 21:37:58 -0300

    Edit: Tabs

--- app/(coffe)/_layout.tsx
+++ app/(coffe)/_layout.tsx
@@ -1,43 +1,56 @@
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { Tabs } from "expo-router";
+import { Tabs } from 'expo-router';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
   return (
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: "#C67C4E",
         tabBarShowLabel: false,
         tabBarStyle: {
+          position: "absolute",
+          bottom: insets.bottom, // ajuste seguro para evitar a barra nativa
+          left: 20,
+          right: 20,
           height: 50,
-          paddingBottom: 4,
-          paddingTop: 4,
+          backgroundColor: "rgba(255, 255, 255, 0.9)", // leve opacidade
+          borderRadius: 30,
+          borderTopWidth: 0,
+          elevation: 0, // sombra no Android
+          shadowColor: "#000", // sombra no iOS
+          shadowOpacity: 0.1,
+          paddingBottom: 10,
+          paddingTop: 10,
         },
       }}
     >
       <Tabs.Screen
         name="home"
         options={{
-          headerShown: false,  // Não exibe o cabeçalho
+          headerShown: false,
           tabBarIcon: ({ color }) => (
-            <MaterialIcons size={32} name="home" color={color} />
+            <MaterialIcons size={28} name="home" color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="favorites"
         options={{
-          headerShown: false,  // Não exibe o cabeçalho
+          headerShown: false,
           tabBarIcon: ({ color }) => (
-            <MaterialIcons size={32} name="favorite" color={color} />
+            <MaterialIcons size={28} name="favorite" color={color} />
           ),
         }}
       />
       <Tabs.Screen
         name="notifications"
         options={{
-          headerShown: false,  // Não exibe o cabeçalho
+          headerShown: false,
           tabBarIcon: ({ color }) => (
-            <MaterialIcons size={32} name="notifications" color={color} />
+            <MaterialIcons size={28} name="notifications" color={color} />
           ),
         }}
       />
